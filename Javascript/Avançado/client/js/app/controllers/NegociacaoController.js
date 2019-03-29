f7e56ed66d56fd4ec6a4e28b228e7fb4f7533417
Jonathas Henrique de Moraes <jonathas.moraes@gsw.com.br>
2019-03-29 19:05:01 -0300

    aula 3 Js A

---
+++ Javascript/Avançado/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,21 @@
+class NegociacaoController{
+    constructor() {
+
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor'); 
+    }
+
+    adiciona(event){
+        event.preventDefault();
+
+        let data = new Date(...
+            this._inputData.value
+                .split('-')
+                .map(function(item, indice){
+                    return item - indice % 2
+                })
+        );
+    }
+}
