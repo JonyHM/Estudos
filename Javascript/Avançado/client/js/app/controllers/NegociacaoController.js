c3c6efc8dd4b7224c97d246dc36d3915e30c7061
Jonathas Henrique de Moraes <jonathas.moraes@gsw.com.br>
2019-04-01 19:00:37 -0300

    aula 5

--- Javascript/Avançado/client/js/app/controllers/NegociacaoController.js
+++ Javascript/Avançado/client/js/app/controllers/NegociacaoController.js
@@ -1,21 +1,33 @@
 class NegociacaoController{
     constructor() {
-
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor'); 
+        this._inputValor = $('#valor');
+        this._form = $('.form');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event){
+        
         event.preventDefault();
+        
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpa();
+        
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+    
+    _limpa(){
+        this._form.reset();
+        this._inputData.focus();
+    }
 
-        let data = new Date(...
-            this._inputData.value
-                .split('-')
-                .map(function(item, indice){
-                    return item - indice % 2
-                })
+    _criaNegociacao(negociacao){
+        return new Negociacao(
+            DateHelper.textToDate(this._inputData.value),            
+            this._inputQuantidade.value,
+            this._inputValor.value
         );
     }
 }
