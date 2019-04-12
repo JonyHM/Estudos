b700411a0c9edf8d35f310225770a18aaf546759
Jonathas Henrique de Moraes <jonathas.moraes@gsw.com.br>
2019-04-12 18:49:34 -0300

    Continuação de AJAX com jQuery

--- jQuery/public/js/frase.js
+++ jQuery/public/js/frase.js
@@ -1,12 +1,41 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria(){
-    $.get("http://localhost:3000/frases", resposta => {
-        
+
+    $("#spinner").show();
+
+    $.get("http://localhost:3000/frases", resposta => {        
         let random = Math.floor(Math.random() * resposta.length);
-        $(".frase").text(resposta[random].texto);
-        
+        $(".frase").text(resposta[random].texto); 
+
         atualizaTamFrase();
         $("#tempo").text(resposta[random].tempo);
-    });
+    })
+    .fail(() => {
+        $("#erro").toggle();
+        setTimeout(() => $("#erro").toggle(), 2000);
+    })
+    .always(() => $("#spinner").toggle());
+}
+
+$("#botao-frase-id").click(buscaFrase);
+
+function buscaFrase(){
+
+    $("#spinner").show();    
+    let fraseId = $("#frase-id").val();
+    let dados = {
+        id: fraseId
+    };
+
+    $.get("http://localhost:3000/frases", dados, resp => {
+        $(".frase").text(resp.texto);
+        atualizaTamFrase();
+        $("#tempo").text(resp.tempo);
+    })
+    .fail(() => {
+        $("#erro").toggle();
+        setTimeout(() => $("#erro").toggle(), 2000);
+    })
+    .always(() => $("#spinner").toggle());
 }
