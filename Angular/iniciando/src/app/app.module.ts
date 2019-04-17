de2652b884d35e37c10cc238d7df090afce9c108
Jonathas Henrique de Moraes <jonathas.moraes@gsw.com.br>
2019-04-17 18:56:49 -0300

    Angular iniciado

---
+++ Angular/iniciando/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { CursosModule } from './cursos/cursos.module';
+import { ComponentInicial } from './init/init.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TesteComponent } from './teste/teste.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentInicial,
+    TesteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CursosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular/iniciando/src/app/init/init.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'component-inicial',
+    template: `
+        <p>Teste de Component</p>
+    `
+})
+export class ComponentInicial {}
