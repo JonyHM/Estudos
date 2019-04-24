d870a1ac26aabca566d059298f3230b7434bef5f
Jonathas H. Moraes <jonathas.moraes@gsw.com.br>
2019-04-24 18:23:07 -0300

    Curso 2 iniciado

--- Angular/alurapic/src/app/app.module.ts
+++ Angular/alurapic/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-
 import { PhotosModule } from './photos/photos.module';
+import { ErrorsModule } from './errors/errors.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,9 @@ import { PhotosModule } from './photos/photos.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PhotosModule
+    PhotosModule,
+    ErrorsModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/alurapic/src/app/photos/photos.module.ts
+++ Angular/alurapic/src/app/photos/photos.module.ts
@@ -1,12 +1,17 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { PhotoComponent } from './photo/photo.component';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoModule } from './photo/photo.module';
 
 @NgModule({
-    declarations: [PhotoComponent],
-    exports: [PhotoComponent],
-    imports: [HttpClientModule]
+    imports: [
+        PhotoModule,
+        PhotoListModule,
+        PhotoFormModule,
+    ]
 })
 export class PhotosModule{
 
